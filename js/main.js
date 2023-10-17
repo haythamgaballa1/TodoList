14d0896acdac2e891f7b34ca653c899621b13171
Haytham gaballa <100927810+haythamgaballa1@users.noreply.github.com>
2023-10-17 22:01:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// jquery
+$(document).ready(function(){ //بعد تحميل العناصر
+    $('.form').change(function(){
+        var task = $(this).val();
+
+        $('#tasks').append('<li>' + task + '<i class="fa-solid fa-check"></i><i class="fa-solid fa-trash"></i> </li>');
+        $('.form')='';
+    })
+    $('ul').on('click','.fa-trash',function(){
+        $(this).parent('li').fadeOut(200)
+    })
+    $('ul').on('click','.fa-check',function(){
+        $(this).parent('li').css({
+            'background-color' : 'greenyellow' ,
+            'border' : '2px green solid'
+        })
+    })
+})
